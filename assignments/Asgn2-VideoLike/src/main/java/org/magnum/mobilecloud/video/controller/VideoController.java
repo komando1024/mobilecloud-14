e40f2c8f67da1815a021a6171f5dfbf8f60ce42b
mejdi <java.user@laposte.net>
2014-09-28 17:05:28 +0200

    findby

--- assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/controller/VideoController.java
+++ assignments/Asgn2-VideoLike/src/main/java/org/magnum/mobilecloud/video/controller/VideoController.java
@@ -1,26 +1,20 @@
 package org.magnum.mobilecloud.video.controller;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Collection;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 
 import org.magnum.mobilecloud.video.client.VideoSvcApi;
 import org.magnum.mobilecloud.video.model.Video;
-import org.magnum.mobilecloud.video.service.VideoFileManager;
 import org.magnum.mobilecloud.video.service.IVideoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.ResourceAccessException;
 
 /**
  * @author mejdi
@@ -52,21 +46,39 @@ public class VideoController {
 		return video;
 	}
 
-//	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.GET)
-//	public void getData(@PathVariable long id, HttpServletResponse response) {
-//		LOGGER.info("getData()");
-//
-//		Video video = videoService.findVideoById(id);
-//		if (video == null) {
-//			throw new ResourceNotFoundException("no video with such id");
-//		}
-//
-//		try {
-//			OutputStream out = response.getOutputStream();
-//			VideoFileManager.get().copyVideoData(video, out);
-//		} catch (IOException e) {
-//			LOGGER.error("problem while downloading file {} ", e.getMessage());
-//			throw new ResourceAccessException("problem while downloading file {}", e);
-//		}
-//	}
+	@RequestMapping(value = VideoSvcApi.VIDEO_TITLE_SEARCH_PATH, method = RequestMethod.GET)
+	public Collection<Video> findVideoByName(@RequestParam(VideoSvcApi.TITLE_PARAMETER) String title){
+		LOGGER.info("findVideoByName()");
+
+		Collection<Video> videos = videoService.findVideoByName(title);
+
+		return videos;
+	}
+
+	@RequestMapping(value = VideoSvcApi.VIDEO_DURATION_SEARCH_PATH, method = RequestMethod.GET)
+	public Collection<Video> findVideoByDuration(@RequestParam(VideoSvcApi.DURATION_PARAMETER) long duration){
+		LOGGER.info("findVideoByDuration()");
+
+		Collection<Video> videos = videoService.findVideoByDuration(duration);
+
+		return videos;
+	}
+
+	//	@RequestMapping(value = VideoSvcApi.VIDEO_SVC_PATH, method = RequestMethod.GET)
+	//	public void getData(@PathVariable long id, HttpServletResponse response) {
+	//		LOGGER.info("getData()");
+	//
+	//		Video video = videoService.findVideoById(id);
+	//		if (video == null) {
+	//			throw new ResourceNotFoundException("no video with such id");
+	//		}
+	//
+	//		try {
+	//			OutputStream out = response.getOutputStream();
+	//			VideoFileManager.get().copyVideoData(video, out);
+	//		} catch (IOException e) {
+	//			LOGGER.error("problem while downloading file {} ", e.getMessage());
+	//			throw new ResourceAccessException("problem while downloading file {}", e);
+	//		}
+	//	}
 }
